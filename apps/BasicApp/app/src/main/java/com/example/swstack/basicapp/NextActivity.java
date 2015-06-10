d358c269584a0ffffc0938447571189f930dd7f9
Stephen Stack <sstack@punchthrough.com>
2015-06-10 14:26:03 -0500

    learning: add another activity with a few buttons and onClick listeners

--- apps/BasicApp/app/src/main/java/com/example/swstack/basicapp/NextActivity.java
+++ apps/BasicApp/app/src/main/java/com/example/swstack/basicapp/NextActivity.java
@@ -1,24 +1,25 @@
 package com.example.swstack.basicapp;
+
 import android.content.Intent;
-import android.view.View;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class NextActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_next);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_next, menu);
         return true;
     }
 
@@ -37,8 +38,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void nextClicked(View view) {
-        System.out.print("Going to next activity!");
+    public void backClicked(View view) {
+        System.out.print("Going back to main activity!");
         Intent intent = new Intent(this, NextActivity.class);
     }
 }
